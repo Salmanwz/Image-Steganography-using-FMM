d7cefa543dfc072acba98eb9eeb1f59ab7c33497
Salmanwz <46998615+Salmanwz@users.noreply.github.com>
2020-08-03 16:59:08 +0530

    Create ImgReciver.py

---
+++ ImgReciver.py
@@ -0,0 +1,8 @@
+import sys
+from recive import *
+ImgRecive=Recive('127.0.0.1',50000)   
+ImgRecive.recive("Test1.png")
+ImgRecive.recive("Test2.png")
+ImgRecive.recive("Test3.png")
+ImgRecive.recive("Test4.png")
+ImgRecive.recive("Test5.png")
